a49b5b248f36026184070688d2d7f9f68bf9fbb1
nero <wnsgur765z@naver.com>
2022-12-07 22:57:34 +0900

    test: Add 동작하는지 기본적인 테스트

--- src/test/java/nero/diary/DiaryApplicationTests.java
+++ src/test/java/nero/diary/DiaryApplicationTests.java
@@ -1,13 +1,38 @@
 package nero.diary;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import nero.diary.domain.user.entity.QUser;
+import nero.diary.domain.user.entity.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class DiaryApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		User user = new User();
+		em.persist(user);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QUser qUser = QUser.user;
+
+		User result = query.selectFrom(qUser).fetchOne();
+
+		assertThat(result).isEqualTo(user);
+
+
+
 	}
 
 }
