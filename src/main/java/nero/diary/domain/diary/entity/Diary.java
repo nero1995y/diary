ef06b8ae48658011c9a4794c02c7a3a0b76f6ff8
nero <wnsgur765z@naver.com>
2022-12-09 23:20:06 +0900

    feat: Add User Entity diary Entity

---
+++ src/main/java/nero/diary/domain/diary/entity/Diary.java
@@ -0,0 +1,34 @@
+package nero.diary.domain.diary.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import nero.diary.domain.user.entity.User;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Diary {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(name = "diary_name")
+    private String name;
+
+    @OneToMany(mappedBy = "diary")
+    private List<User> users = new ArrayList<>();
+
+    public Diary(String name, List<User> users) {
+        this.name = name;
+        this.users = users;
+    }
+
+    public Diary(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/nero/diary/domain/user/entity/User.java
+++ src/main/java/nero/diary/domain/user/entity/User.java
@@ -1,18 +1,40 @@
 package nero.diary.domain.user.entity;
 
+import lombok.AccessLevel;
+import nero.diary.domain.diary.entity.Diary;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
 public class User {
 
     @Id
     @GeneratedValue
     @Column(name = "user_id")
     private Long id;
+
+    @Column(name = "user_name")
+    private String username;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diary_id")
+    private Diary diary;
+
+    public User(String username, Diary diary) {
+        this.username = username;
+        
+        if(diary != null) {
+            changeDiary(diary);
+        }
+    }
+
+    public void changeDiary(Diary diary) {
+        this.diary = diary;
+        diary.getUsers().add(this);
+    }
 }
