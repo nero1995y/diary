0f8ce2d27e4c486daf2698fdeab2b85a287d3979
nero <wnsgur765z@naver.com>
2022-12-14 22:07:35 +0900

    feat: Add user entity 컬럼 추가

--- src/main/java/nero/diary/domain/user/entity/User.java
+++ src/main/java/nero/diary/domain/user/entity/User.java
@@ -22,13 +22,26 @@ public class User {
     @Column(name = "user_name")
     private String username;
 
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "user_phone")
+    private String phone;
+
+    @Column(name = "user_password")
+    private String password;
+
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "diary_id")
     private Diary diary;
 
     @Builder
-    public User(String username, Diary diary) {
+    public User(String username, String email, String phone, String password, Diary diary) {
         this.username = username;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
         
         if(diary != null) {
             changeDiary(diary);
