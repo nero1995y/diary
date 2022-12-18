61581cc125236bddb083877f51d2181e003ee44a
nero <wnsgur765z@naver.com>
2022-12-18 23:28:49 +0900

    feat: Add user update dto

---
+++ src/main/java/nero/diary/domain/user/dto/UserUpdateRequestDto.java
@@ -0,0 +1,36 @@
+package nero.diary.domain.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import nero.diary.domain.user.entity.User;
+
+@Getter
+@NoArgsConstructor
+public class UserUpdateRequestDto {
+    private String username;
+    private String email;
+    private String password;
+    private String phone;
+
+    @Builder
+    public UserUpdateRequestDto(String username,
+                                String email,
+                                String password,
+                                String phone) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .username(this.username)
+                .email(this.email)
+                .password(this.password)
+                .phone(this.phone)
+                .build();
+
+    }
+}
