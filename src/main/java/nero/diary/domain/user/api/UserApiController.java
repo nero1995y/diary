7139b0c05528916c6fafd803fb8eeb62479efc7a
nero <wnsgur765z@naver.com>
2022-12-19 21:51:17 +0900

    feat: Add user api crud

--- src/main/java/nero/diary/domain/user/api/UserApiController.java
+++ src/main/java/nero/diary/domain/user/api/UserApiController.java
@@ -1,12 +1,13 @@
 package nero.diary.domain.user.api;
 
 import lombok.RequiredArgsConstructor;
+import nero.diary.domain.user.dto.UserResponseDto;
 import nero.diary.domain.user.dto.UserSaveRequestDto;
+import nero.diary.domain.user.dto.UserUpdateRequestDto;
+import nero.diary.domain.user.dto.UsersResponseDto;
 import nero.diary.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -20,6 +21,36 @@ public class UserApiController {
     public ResponseEntity<Void> register(@RequestBody @Valid UserSaveRequestDto requestDto) {
         userService.register(requestDto);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("api/v1/users")
+    public ResponseEntity<UsersResponseDto> findList() {
+        UsersResponseDto response = userService.findAll();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("api/v1/user/{username}")
+    public ResponseEntity<UserResponseDto> findByUser(@PathVariable String username){
+
+        UserResponseDto response = userService.findUser(username);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("api/v1/user/{username}")
+    public ResponseEntity<Void> update(@PathVariable String username,
+                                       @RequestBody @Valid UserUpdateRequestDto requestDto){
+
+        userService.update(username, requestDto);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("api/v1/user/{username}")
+    public ResponseEntity<Void> delete(@PathVariable String username){
+        userService.remove(username);
+        return  ResponseEntity.ok().build();
     }
 
 }
