e4cd14a690aa7e6f92c72c74748ccac7ea0d1ae1
nero <wnsgur765z@naver.com>
2023-01-04 09:24:45 +0900

    fix: Change user tabale 이름변경

--- src/main/java/nero/diary/domain/user/entity/User.java
+++ src/main/java/nero/diary/domain/user/entity/User.java
@@ -11,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "USERS")
 public class User {
 
     @Id
--- src/test/java/nero/diary/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/nero/diary/domain/user/repository/UserRepositoryTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,7 +14,6 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
     @Autowired
