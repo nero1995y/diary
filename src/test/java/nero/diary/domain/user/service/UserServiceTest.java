3d569fd62c4368ea12efe2a7682dd14f1be6dac5
nero <wnsgur765z@naver.com>
2023-01-06 16:42:33 +0900

    chore: Add test yml 설정 분리

--- src/test/java/nero/diary/domain/user/service/UserServiceTest.java
+++ src/test/java/nero/diary/domain/user/service/UserServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -24,6 +25,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 
 @SpringBootTest
+@TestPropertySource("classpath:application.yml")
 @Transactional
 class UserServiceTest {
 
