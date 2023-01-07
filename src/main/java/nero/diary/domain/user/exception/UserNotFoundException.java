266a4cdede903538fb6390e247431b793144ed95
nero <wnsgur765z@naver.com>
2023-01-07 22:41:09 +0900

    feat: Add UserNotFoundException 메세지

--- src/main/java/nero/diary/domain/user/exception/UserNotFoundException.java
+++ src/main/java/nero/diary/domain/user/exception/UserNotFoundException.java
@@ -1,4 +1,11 @@
 package nero.diary.domain.user.exception;
 
 public class UserNotFoundException extends RuntimeException{
+
+    private static final String MESSAGE = "존재하지 않는 유저입니다.";
+
+    public UserNotFoundException() {
+        super(MESSAGE);
+    }
+
 }
