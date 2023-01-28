b1c515119ff3f100b023677d353639017af9dd43
nero <wnsgur765z@naver.com>
2023-01-28 21:13:09 +0900

    test: deloy script 수정

---
+++ scripts/start.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT="/home/ec2-user/app/step2"
+JAR_FILE="$PROJECT_ROOT/diary.jar"
+
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+# build 파일 복사
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+
+# jar 파일 실행
+echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml,classpath:/application-real.yml \
+  -Dspring.profiles.active=real $PROJECT_ROOT/nohup.out 2>&1 &
+
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
