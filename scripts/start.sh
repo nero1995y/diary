16fc828619495e32adea6c3f0818af0ad5781df1
nero <wnsgur765z@naver.com>
2023-02-01 18:43:20 +0900

    test: 무중단 배포

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,7 +13,7 @@ TIME_NOW=$(date +%c)
 echo "$TIME_NOW > Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar REPOSITORY/"
 
-cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
